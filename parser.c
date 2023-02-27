4d749db0d8861bbfb28b1c3f5cfd1de80bee0d1e
Pranav-Taneja <taneja21pranav@gmail.com>
2023-02-27 19:27:33 +0530

    file handling

--- parser.c
+++ parser.c
@@ -1,11 +1,19 @@
-
+#include<stdio.h>
+#include<stdlib.h>
 
 int NUM_TERM;
 int NUM_NONTERM;
 int NUM_GRAMRULES;
 
-int main(){
+int ifNT(char *token)
+{
+    if(*token=='<')
+        return 1;
+    return 0;         
+}
 
+int main()
+{
     int grammar[NUM_GRAMRULES][10];
     for(int i = 0; i<NUM_GRAMRULES; i++)
         for(int j = 0; j<10; j++)
@@ -18,5 +26,15 @@ int main(){
                 F[i][j][k] = -1;
         }
     }
-
+FILE *gram = fopen("grammar.txt","r");
+char ch;
+int numLines = 0;
+while(ch!=EOF)
+{
+    ch = fegtc(gram);
+    if(ch='\n')
+        numLines++;
+}
+printf("%d\n",numLines);
+return 0;
 }
