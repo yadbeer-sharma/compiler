5bc2890e3d67e700d78170c381a66ecb3d6333bd
Pranav-Taneja <taneja21pranav@gmail.com>
2023-02-27 19:38:48 +0530

    file handling

--- parser.c
+++ parser.c
@@ -6,6 +6,18 @@ int NUM_TERM;
 int NUM_NONTERM;
 int NUM_GRAMRULES;
 
+int fileNumLines(FILE *fp)
+{
+    char ch;
+    int numLines = 1;
+    while((ch=fgetc(fp))!=EOF)
+    {
+        if(ch=='\n')
+            numLines++;
+    }
+    return numLines;
+}
+
 int ifNT(char *token)
 {
     if(*token=='<')
@@ -15,27 +27,20 @@ int ifNT(char *token)
 
 int main()
 {
-    // int grammar[NUM_GRAMRULES][10];
-    // for(int i = 0; i<NUM_GRAMRULES; i++)
-    //     for(int j = 0; j<10; j++)
-    //         grammar[i][j] = -1;
+    int grammar[NUM_GRAMRULES][10];
+    for(int i = 0; i<NUM_GRAMRULES; i++)
+        for(int j = 0; j<10; j++)
+            grammar[i][j] = -1;
 
-    // int F[NUM_NONTERM][2][NUM_TERM];
-    // for(int i = 0; i<NUM_NONTERM; i++){
-    //     for(int j = 0; j<2; j++){
-    //         for(int k = 0; k<NUM_TERM; k++)
-    //             F[i][j][k] = -1;
-    //     }
-    // }
-FILE *gram = fopen("grammar.txt","r");
-char ch;
-int numLines = 1;
-while((ch=fgetc(gram))!=EOF)
-{
-    //ch = fgetc(gram);
-    if(ch=='\n')
-        numLines++;
-}
-printf("%d\n",numLines);
-return 0;
+    int F[NUM_NONTERM][2][NUM_TERM];
+    for(int i = 0; i<NUM_NONTERM; i++){
+        for(int j = 0; j<2; j++){
+            for(int k = 0; k<NUM_TERM; k++)
+                F[i][j][k] = -1;
+        }
+    }
+    FILE *gram = fopen("grammar.txt","r");
+    char* tokBuf;
+    int numLines = 1;
+    return 0;
 }
