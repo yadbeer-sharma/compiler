6e0c531235a55e7bacdd3f3364074c592786fe76
Yadbeer Sharma <f20190521@pilani.bits-pilani.ac.in>
2023-02-27 19:42:36 +0530

    added F and G structures

--- parser.c
+++ parser.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int NUM_TERM;
 int NUM_NONTERM;
@@ -7,34 +8,36 @@ int NUM_GRAMRULES;
 
 int ifNT(char *token)
 {
-    if(*token=='<')
+    if (*token == '<')
         return 1;
-    return 0;         
+    return 0;
 }
 
 int main()
 {
-    int grammar[NUM_GRAMRULES][10];
-    for(int i = 0; i<NUM_GRAMRULES; i++)
-        for(int j = 0; j<10; j++)
+    int grammar[NUM_GRAMRULES][15];
+    for (int i = 0; i < NUM_GRAMRULES; i++)
+        for (int j = 0; j < 15; j++)
             grammar[i][j] = -1;
 
     int F[NUM_NONTERM][2][NUM_TERM];
-    for(int i = 0; i<NUM_NONTERM; i++){
-        for(int j = 0; j<2; j++){
-            for(int k = 0; k<NUM_TERM; k++)
+    for (int i = 0; i < NUM_NONTERM; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            for (int k = 0; k < NUM_TERM; k++)
                 F[i][j][k] = -1;
         }
     }
-FILE *gram = fopen("grammar.txt","r");
-char ch;
-int numLines = 0;
-while(ch!=EOF)
-{
-    ch = fegtc(gram);
-    if(ch='\n')
-        numLines++;
-}
-printf("%d\n",numLines);
-return 0;
+    FILE *gram = fopen("grammar.txt", "r");
+    char ch;
+    int numLines = 0;
+    while (ch != EOF)
+    {
+        ch = fegtc(gram);
+        if (ch = '\n')
+            numLines++;
+    }
+    printf("%d\n", numLines);
+    return 0;
 }
